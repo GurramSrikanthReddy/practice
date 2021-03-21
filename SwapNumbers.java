08264d30b38ac76ec5dd737a3a25dad3f423ecf0
GurramSrikanthReddy <gurramsrikanthreddy12@gmail.com>
2021-03-21 12:14:27 +0530

    swap numbers

---
+++ SwapNumbers.java
@@ -0,0 +1,17 @@
+public class SwapNumbers{
+
+	public static void main(String args[]){
+
+	int a=20,b=30,c; //c is the temperory varaible
+
+        c = b;
+        b = a;
+        a = c;
+System.out.println("The value of a : " +a);
+System.out.println("The value of b : " +b);
+
+
+
+
+	}
+}
