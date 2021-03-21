8b06bf3f49c8f255fde04e336ac83c0dde78fcde
GurramSrikanthReddy <gurramsrikanthreddy12@gmail.com>
2021-03-21 12:19:25 +0530

    if

---
+++ IfDemo.java
@@ -0,0 +1,16 @@
+public class IfDemo{
+
+	public static void main(String args[]){
+		
+  int years = 6;
+  double salary = 45000;
+      if(years > 5)
+System.out.println("The net bonus : " +(salary*5)/100);
+else
+ System.out.println("No Bonus");
+
+
+
+	}
+
+}
