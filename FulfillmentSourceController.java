70a75b6d50341f83e3535246f5838d64f47fef7e
nitishareddy19 <nitisha.ambati@gmail.com>
2021-06-29 22:07:35 +0530

    stroy116

--- FulfillmentSourceController.java
+++ FulfillmentSourceController.java
@@ -13,13 +13,7 @@ public static void main(String args[]){
 	    System.out.println("*****************");
 	   	FulfillmentSource f2 = new FulfillmentSource();
 
-	    System.out.println(f2.nodeId);
-	    System.out.println(f2.actualInventoryNode);
-	    System.out.println(f2.quantity);
-
-
-		System.out.println("main methods ends");
-	
+	    
 
 
 }
